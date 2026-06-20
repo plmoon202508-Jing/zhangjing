08e47a8e1e88c120455be62b21ec9f0f9f3224be
LUYAN <luyan@192.168.1.132>
2026-06-20 17:47:58 +0800

    修复ConstellationScreen.kt的Compose imports

--- android/app/src/main/java/com/asiainfo/satellite/ui/screens/ConstellationScreen.kt
+++ android/app/src/main/java/com/asiainfo/satellite/ui/screens/ConstellationScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
